df750d4dc6c7412487b46446847f8599c80c8110
karur4n <karuran.f@gmail.com>
2016-12-18 01:02:25 +0900

    minimal

---
+++ src/components/Item.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :class="classObject">
+    <input
+      type="checkbox"
+      :checked="completed"
+      @change="toggle"
+    >
+    {{ title }}
+    <button @click.prevent="remove">
+      [x]
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'item',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    completed: {
+      type: Boolean,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    classObject: function () {
+      return {
+        item: true,
+        isCompleted: this.completed
+      }
+    }
+  },
+  methods: {
+    remove: function () {
+      this.$emit('remove', this.id)
+    },
+    toggle: function () {
+      this.$emit('toggle', this.id)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.item.isCompleted {
+  text-decoration: line-through;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="list">
+    <item
+      v-for="(todo, index) in todos"
+      :id="index"
+      :title="todo.title"
+      :completed="todo.completed"
+      @remove="removeItem"
+      @toggle="toggleItem"
+    ></item>
+  </div>
+</template>
+
+<script>
+import Item from './Item'
+
+export default {
+  name: 'list',
+  components: {
+    Item
+  },
+  data () {
+    return {
+      todos: [
+        { title: 'Buy a milk', completed: false },
+        { title: 'Clean the kitchen', completed: false },
+        { title: 'Do homework', completed: true }
+      ]
+    }
+  },
+  methods: {
+    removeItem: function (itemIndex) {
+      this.todos.splice(itemIndex, 1)
+    },
+    toggleItem: function (itemIndex) {
+      this.todos = this.todos.map((todo, idx) => {
+        if (itemIndex !== idx) return todo
+        todo.completed = !todo.completed
+        return todo
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
